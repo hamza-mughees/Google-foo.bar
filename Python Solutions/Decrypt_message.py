580800a17191c944c35f5c77926ccd3356b93400
hamza-mughees <mugheesh@tcd.ie>
2020-05-31 14:36:13 +0100

    Decrypted the encrypted message

---
+++ Python Solutions/Decrypt_message.py
@@ -0,0 +1,15 @@
+import base64
+
+# key is your username
+key = "hamzamughees"
+
+# encryption is your message
+encryption = "E0YeDwIOEBQbQkVJSEYKCAQMAUBERUIQBw0BHwAKAAJPRV9TTwQeDgQIGAIMQklTTwQLHA4fARRP RV9TTwgDGRMIEQ4KCQBUREFKGwIFHAIeAAgWBhVKWltNUhIGCQoQAwQJXU1NUhUJBwcaHBJKWltN UhQJAwBUREFKHA4CUkdSRUIEAQ9MXRw="
+
+decryption = []
+
+for i, j in enumerate(base64.b64decode(encryption)):
+    ch = chr(j ^ ord(key[i % len(key)]))
+    decryption.append(ch)
+
+print(''.join(decryption))
